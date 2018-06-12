07d8658c715183ff5f0003d7574a8a70c8d94d96
Vincent <vcwang@ucsc.edu>
2018-06-11 18:04:07 -0700

    dependecies added

---
+++ static/js/editor.vue
@@ -0,0 +1,16 @@
+import quill from 'vue-quill-editor'
+
+export default {
+  data () {
+    return {
+      name: 'app',
+      content: '<h2>Example</h2>',
+      editorOption: {}
+    }
+  },
+  components: {
+    Hello,
+    quillEditor
+  }
+}
+</script>
