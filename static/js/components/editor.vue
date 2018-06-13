2fc9e90d2f8763b7a48a5bd1ea7339ab083244d1
Vincent <vcwang@ucsc.edu>
2018-06-12 18:14:50 -0700

    just adding files for compatibility  Please enter the commit message for your changes. Lines starting

---
+++ static/js/components/editor.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="quill-editor">
+    <slot name="toolbar"></slot>
+    <div ref="editor"></div>
+  </div>
+</template>
+
+
+<script>
+import { quillEditor } from 'vue-quill-editor'
+
+export default {
+  data () {
+    return {
+      name: '',
+      content: '<h2>Example</h2>',
+      editorOption: {}
+    }
+  },
+  components: {
+    Hello,
+    quillEditor
+  }
+}
+</script>
