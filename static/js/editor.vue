88d39b6612d66bccdf2fdf5bc0519d17330c6f52
Vincent <vcwang@ucsc.edu>
2018-06-12 18:17:56 -0700

    more files

--- static/js/editor.vue
+++ static/js/editor.vue
@@ -1,9 +1,18 @@
-import quill from 'vue-quill-editor'
+<template>
+  <div class="quill-editor">
+    <slot name="toolbar"></slot>
+    <div ref="editor"></div>
+  </div>
+</template>
+
+
+<script>
+import { quillEditor } from 'vue-quill-editor'
 
 export default {
   data () {
     return {
-      name: 'app',
+      name: '',
       content: '<h2>Example</h2>',
       editorOption: {}
     }
